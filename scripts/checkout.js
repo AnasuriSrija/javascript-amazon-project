f9932349c180c59ad4f3fa8c2ffbf93e9c49dadb
SrijaAnasuri <srija.anasuri@gmail.com>
2025-09-01 17:17:49 -0700

    Delete products from the HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -12,7 +12,7 @@ cart.forEach((cartItem) => {
         }
     });
 cartSummaryHTML +=  `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 22
             </div>
@@ -97,6 +97,8 @@ document.querySelectorAll('.js-delete-quantity')
         const productId = link.dataset.productId;
         removeFromCart(productId);
         //console.log(cart);
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     })
 
-})
+});
